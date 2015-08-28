587dd2bb62d27fe23ea20aad67ae21985ce3bdcc
Ray <rwang238@bloomberg.net>
2015-08-28 20:24:28 +0000

    chef vault mysql hive password

--- cookbooks/bcpc-hadoop/recipes/hive_config.rb
+++ cookbooks/bcpc-hadoop/recipes/hive_config.rb
@@ -4,10 +4,27 @@
 # Description : To setup hive configuration only. No hive package will be installed through this Recipe
 #
 
+# check and create the data bag "hadoop"
+create_databag("hadoop")
 
-#Create hive password
-make_config('mysql-hive-password', secure_password)
+#Create hive password with back compatiblity
+mysql_hive_password = get_config("mysql-hive-password")
+if mysql_hive_password.nil?
+  mysql_hive_password = secure_password
+end
 
+bootstrap = get_all_nodes.select{|s| s.hostname.include? 'bootstrap'}[0].fqdn
+results = get_nodes_for("hive_config").map!{ |x| x['fqdn'] }.join(",")
+nodes = results == "" ? node['fqdn'] : results
+
+chef_vault_secret "mysql-hive" do
+  data_bag 'hadoop'
+  raw_data({ 'password' => mysql_hive_password })
+  admins "#{ nodes },#{ bootstrap }"
+  search '*:*'
+  action :nothing
+end.run_action(:create_if_missing)
+  
 %w{hive webhcat hcat hive-hcatalog}.each do |w|
   directory "/etc/#{w}/conf.#{node.chef_environment}" do
     owner "root"
