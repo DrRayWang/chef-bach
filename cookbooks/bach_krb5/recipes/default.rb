00120891fd214de9383c2149c3274b45834ee62a
Ray <rwang238@bloomberg.net>
2015-09-30 11:59:27 -0400

    chef vault kerberos master and admin passwords

--- cookbooks/bach_krb5/recipes/default.rb
+++ cookbooks/bach_krb5/recipes/default.rb
@@ -1,7 +1,23 @@
 # Set master passwords
-make_config("krb5-master-password", secure_password)
-make_config("krb5-admin-password", secure_password)
+create_databag('configs')
+bootstrap = get_bootstrap
+
+chef_vault_secret "krb5-master" do
+  data_bag 'os'
+  raw_data({ 'password' => secure_password })
+  admins bootstrap
+  search '*:*'
+  action :nothing
+end.run_action(:create_if_missing)
+
+chef_vault_secret "krb5-admin" do
+  data_bag 'os'
+  raw_data({ 'password' => secure_password })
+  admins bootstrap
+  search '*:*'
+  action :nothing
+end.run_action(:create_if_missing)
 
 # Override password related node attributes
-node.override['krb5']['master_password'] = get_config("krb5-master-password")
-node.override['krb5']['admin_password'] = get_config("krb5-admin-password")
+node.override['krb5']['master_password'] = get_config!("password","krb5-master","os")
+node.override['krb5']['admin_password'] = get_config!("password","krb5-admin","os")
